019c1bc55e40d29b9ffe394b5dad80ba8ad7dff9
talha828 <talha.developer.01@gmail.com>
2024-05-26 20:55:13 +0500

    quick fix

--- lib/utilities/utilities.dart
+++ lib/utilities/utilities.dart
@@ -1,5 +1,5 @@
 
 
 // TODO : Add Your API KEY
-String apiKey = "AIzaSyCbxUpGHl1KAgUC1NJpDM3Fqbz0mUrBnl4";
+String apiKey = "Add Your API";
 String baseURL = "https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=$apiKey";
